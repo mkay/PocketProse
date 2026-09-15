5d870eca3314a1ddbe284d0ea5fc55f310939886
mk <mail@singular.de>
2026-09-15 21:55:27 +0300

    Pocket Prose 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.singular.writer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
     }
 
     buildTypes {
